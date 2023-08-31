7e9dec045c75566ae835743eef517807d24eb951
johnyeskandar <johnyeskandar@localhost>
2023-08-31 10:45:05 -0500

    Message

--- src/studio0/Message.java
+++ src/studio0/Message.java
@@ -3,6 +3,6 @@ package studio0;
 public class Message {
 
 	public static void main(String args[]) {
-		System.out.println("Say hi to your group mates!");
+		System.out.println("Hi guys, how has your day been? I am very hungry.");
 	}
 }
